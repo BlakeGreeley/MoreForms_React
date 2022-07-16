489ec242acf45e474d0229264fdfab4f81b557f6
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-07-15 17:15:08 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import Form from './components/Form';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='form'>Form</h1>
+      <Form> 
+        
+      </Form>
+    </div>
+  );
+}
+
+export default App;
